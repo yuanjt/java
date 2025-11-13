3c5d079aada4229afbfadcf1ce59b3f2b69cca99
yuanjt <terry.yuanjt@gmail.com>
2025-11-13 14:45:23 +0800

    reformat project

---
+++ java90/src/main/java/Hello.java
@@ -0,0 +1,6 @@
+/** Created by IntelliJ IDEA. @Author: Yuan Jiangtao @Created: 2025/11/13 14:42 */
+public class Hello {
+    public static void main(String[] args){
+    System.out.println("Hello @TerryYuanjt");
+    }
+}
